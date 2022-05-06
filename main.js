78465d1e32703960e90b65c321d53a55cc19bd35
rockstararyaman <93260229+rockstararyaman@users.noreply.github.com>
2022-05-06 17:25:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,30 @@ recognition.onresult = function(event) {
 console.log(event);
 
 var Content = event.results[0][0].transcript;
-console.log(Content);
 
-document.getElementById("textbox").innerHTML = Content;
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    speak();
+    
 }
+
+
+
+function speak() {
+     var synth = window.speechSynthesis;
+
+     speak_data = document.getElementById("textbox").value;
+
+     var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+     synth.speak(utterThis);
+     Webcam.attach(camera);
+}
+
+Webcam.set({
+width:360,
+height:250,
+image_format : 'png' ,
+png_quality:90
+})
+camera = document.getElementById("camera");
