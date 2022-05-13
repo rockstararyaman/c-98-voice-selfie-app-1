be4bf18e99d3ee3bdb4eb1a2059fb85f90a67bd7
rockstararyaman <93260229+rockstararyaman@users.noreply.github.com>
2022-05-13 17:31:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,8 +15,11 @@ var Content = event.results[0][0].transcript;
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
+    if(Content =="take my selfie")
+    {
+    console.log("taking selfie --- ");
     speak();
-    
+    }
 }
 
 
@@ -24,12 +27,19 @@ var Content = event.results[0][0].transcript;
 function speak() {
      var synth = window.speechSynthesis;
 
-     speak_data = document.getElementById("textbox").value;
+     speak_data = "Taking your selfie in 5 seconds";
 
      var utterThis = new SpeechSynthesisUtterance(speak_data);
 
      synth.speak(utterThis);
      Webcam.attach(camera);
+
+     setTimeout(function()
+     {
+         take_snapshot();
+         save();
+     }, 5000);
+    
 }
 
 Webcam.set({
@@ -39,3 +49,16 @@ image_format : 'png' ,
 png_quality:90
 })
 camera = document.getElementById("camera");
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save() {
+link = document.getElementById("link");
+image = document.getElementById("selfie_image").src ;
+link.href = image;
+link.click();
+}
